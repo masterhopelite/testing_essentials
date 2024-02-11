dd1bc785c179f9a7949f476994608a908fae55d6
rohit singh <106547664+masterhopelite@users.noreply.github.com>
2024-02-12 01:44:05 +0530

    Add files via upload
    sdf

--- example.java
+++ example.java
@@ -1,11 +1,18 @@
 package introduction;
 
+import java.time.Duration;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class example {
 
@@ -16,6 +23,9 @@ public class example {
 		options.addArguments("--remote-allow-origins=*");
 		ChromeDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//		WebDriverWait wait = new WebDriverWait(driver,30);
+//		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'COMPOSE')]")));
 		
 		String newTabUrl = "https://www.linkedin.com/home";
 		String newTabUrl1 = "https://www.google.com";
